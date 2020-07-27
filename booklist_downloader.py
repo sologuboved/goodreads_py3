5d7c0963d131e6c5f2b593122a92567494dc9b21
sologuboved <sologuboved@gmail.com>
2020-07-27 14:39:08 +0300

    added years

--- booklist_downloader.py
+++ booklist_downloader.py
@@ -1,6 +1,7 @@
+import re
 import requests
 from bs4 import BeautifulSoup
-from helpers import dump_utf_json, load_utf_json
+from helpers import dump_utf_json, load_utf_json, which_watch, counter
 
 
 def download_raw_booklist(url, num_pages, target_json):
@@ -21,13 +22,53 @@ def download_raw_booklist(url, num_pages, target_json):
 def process_booklist(src_json, target_txt):
     with open(target_txt, 'wt') as handler:
         handler.write('<ol>')
-        for author_name, title, title_url in load_utf_json(src_json):
-            handler.write('<li><a href="{}">{} - {}</a></li>'.format('https://www.goodreads.com' + title_url,
-                                                                       author_name, title))
+        for author_name, title, title_url, year in load_utf_json(src_json):
+            handler.write('<li>{} - <a href="{}">{}</a> ({})</li>'.format(author_name, get_full_url(title_url),
+                                                                          title, year))
         handler.write('</ol>')
+
+
+def get_full_url(url):
+    return 'https://www.goodreads.com' + url
+
+
+@which_watch
+def add_first_publication_years(src_json):
+    books = load_utf_json(src_json)
+    count = counter(len(books))
+    for book in books:
+        next(count)
+        book.append(get_first_publication_year(book[-1]))
+    print("\nDumping {} yeared books...".format(len(books)))
+    dump_utf_json(books, src_json)
+
+
+def get_first_publication_year(book_url):
+    book_url = get_full_url(book_url)
+    soup = BeautifulSoup(requests.get(book_url).content, 'lxml')
+    year = None
+    try:
+        year = soup.find('nobr', {'class': 'greyText'}).text.strip()
+    except AttributeError:
+        try:
+            details = soup.find_all('div', {'id': 'details'})[0].find_all('div', {'class': 'row'})
+        except IndexError:
+            pass
+        else:
+            for row in details:
+                try:
+                    year = re.findall(r"Published.+?(\d\d\d\d)", row.text, flags=re.DOTALL)[0]
+                except IndexError:
+                    continue
+                break
+    else:
+        year = re.findall(r'\d\d\d\d', year)[0]
+    return year
 
 
 if __name__ == '__main__':
     json_fname = 'raw_booklist.json'
     # download_raw_booklist('https://www.goodreads.com/list/show/148119.Conspirologic?page={}', 2, json_fname)
     process_booklist(json_fname, 'booklist.txt')
+    # add_first_publication_years(json_fname)
+    # get_first_publication_year('/book/show/3327825-the-mythology-of-the-secret-societies')
--- helpers.py
+++ helpers.py
@@ -1,8 +1,42 @@
-# -*- coding: utf-8 -*-
-
 import json
+import time
 from math import floor, sqrt
+from functools import wraps
 from global_vars import *
+
+
+def which_watch(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+
+        def report_time():
+            print("\n{} took {}\n".format(func.__name__,
+                                          time.strftime("%H:%M:%S", time.gmtime(time.perf_counter() - start))))
+
+        start = time.perf_counter()
+        try:
+            result = func(*args, **kwargs)
+        except BaseException as e:
+            raise e
+        else:
+            return result
+        finally:
+            report_time()
+
+    return wrapper
+
+
+def counter(total=None, every=1):
+    count = 0
+    if total:
+        postfix = " / {}".format(total)
+    else:
+        postfix = str()
+    while True:
+        count += 1
+        if count == 1 or count == total or not count % every:
+            print("\r{}{}".format(count, postfix), end=str(), flush=True)
+        yield count
 
 
 def load_utf_json(json_file):
